9db890b9b14943ca732eb76e954ae70eafe75d18
OlhaOs <olalaadka@gmail.com>
2023-04-18 09:48:29 +0300

    modal on useEffect

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,34 +1,60 @@
 import css from './Modal.module.css';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeydown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onCloseModal);
-  }
-  handleKeydown = e => {
+export default function Modal({ children, onClose }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeydown);
+    window.removeEventListener('keydown', onCloseModal);
+  });
+
+  const handleKeydown = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  onCloseModal = e => {
+  const onCloseModal = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <div className={css.modalBackdrop} onClick={this.onCloseModal}>
-        <div className={css.modalContent}>{this.props.children}</div>
-      </div>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <div className={css.modalBackdrop} onClick={onCloseModal}>
+      <div className={css.modalContent}>{children}</div>
+    </div>,
+    modalRoot
+  );
 }
+
+// export default class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeydown);
+//   }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.onCloseModal);
+//   }
+//   handleKeydown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   onCloseModal = e => {
+//     if (e.target === e.currentTarget) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <div className={css.modalBackdrop} onClick={this.onCloseModal}>
+//         <div className={css.modalContent}>{this.props.children}</div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
